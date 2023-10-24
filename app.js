a4534ba7a6a3732cc237910bae2f1db7073344ef
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-24 07:17:45 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// Question no 1
+
+// function sevenboo(arr){
+//     arr.map(x=>{
+//         if(x === 7){
+//             console.log('Boom');
+//         }
+//     })
+// }
+            
+// sevenboo([1,2,5,3,3,7,8])
+    
+
+
+// Question no 2
+
+// function counttrue(arr) {
+//     var num;
+//     var realnum = arr.filter(x => x=== true)
+//     if(realnum.length){
+//         console.log(2);
+//     } else {
+//         console.log(0);
+//     }
+// }
+
+// counttrue([])
+
+
+
+// Question no 3
+
+// function redundant(str){
+//     return str
+// }
+
+// var f1 = redundant('Zain')
+// console.log(f1);
+
+// var f2 = redundant('Khan')
+// console.log(f2);
+// var f3 = redundant('apple')
+// console.log(f3);
+
+
+
+// function possibleBonus(p1, p2){
+//     if(p1 >= p2){
+//         return console.log(false, "kyon k player one agay hai player 2 se");;
+//     } else{
+//         if( (p1 + 6) >= p2 ){
+//             return console.log(true, "Player one player two ko kill kar sakta hai");;
+//         } else{
+//             return console.log(false, "abhi bht dor hai");
+//         }
+//     }
+// }
+
+// possibleBonus(5,3)
+
