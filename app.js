4eed110ce24db5e03bb3bf5ad3eaf8166fbb767e
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-25 10:32:35 +0500

    Data added.

--- app.js
+++ app.js
@@ -1,60 +1,6 @@
-// Question no 1
+// What is the result of the following expression?
 
-// function sevenboo(arr){
-//     arr.map(x=>{
-//         if(x === 7){
-//             console.log('Boom');
-//         }
-//     })
-// }
-            
-// sevenboo([1,2,5,3,3,7,8])
-    
+2 + "2"
 
-
-// Question no 2
-
-// function counttrue(arr) {
-//     var num;
-//     var realnum = arr.filter(x => x=== true)
-//     if(realnum.length){
-//         console.log(2);
-//     } else {
-//         console.log(0);
-//     }
-// }
-
-// counttrue([])
-
-
-
-// Question no 3
-
-// function redundant(str){
-//     return str
-// }
-
-// var f1 = redundant('Zain')
-// console.log(f1);
-
-// var f2 = redundant('Khan')
-// console.log(f2);
-// var f3 = redundant('apple')
-// console.log(f3);
-
-
-
-// function possibleBonus(p1, p2){
-//     if(p1 >= p2){
-//         return console.log(false, "kyon k player one agay hai player 2 se");;
-//     } else{
-//         if( (p1 + 6) >= p2 ){
-//             return console.log(true, "Player one player two ko kill kar sakta hai");;
-//         } else{
-//             return console.log(false, "abhi bht dor hai");
-//         }
-//     }
-// }
-
-// possibleBonus(5,3)
-
+// Options:
+// A) "22"  B) 4  C) "4"  D) 22
---
+++ data.js
@@ -0,0 +1,145 @@
+// ### Quiz Question 1:
+
+// // What will the output of the following code be?
+// function foo() {
+//   var a = 1;
+//   function bar() {
+//     var b = 2;
+//     console.log(a + b);
+//   }
+//   bar();
+// }
+// foo();
+
+// Options:
+// A) 3  
+// B) 2  
+// C) 1  
+// D) Error  
+
+// ### Quiz Question 2:
+
+// // What is the result of the following expression?
+// 2 + "2"
+
+// Options:
+// A) "22"  
+// B) 4  
+// C) "4"  
+// D) 22  
+
+// ### Quiz Question 3:
+
+// // What will be logged to the console?
+// var x = 1;
+// function test() {
+//   console.log(x);
+//   var x = 2;
+// }
+// test();
+
+// Options:
+// A) 1  
+// B) 2  
+// C) undefined  
+// D) ReferenceError  
+
+// ### Quiz Question 4:
+
+// // What will the output be?
+// var a = 10;
+// function example() {
+//   console.log(a);
+//   var a = 20;
+// }
+// example();
+
+// Options:
+// A) 10  
+// B) 20  
+// C) undefined  
+// D) ReferenceError  
+
+// ### Quiz Question 5:
+
+// // What does the following expression evaluate to?
+// typeof typeof 1
+
+// Options:
+// A) "number"  
+// B) "string"  
+// C) "object"  
+// D) "undefined"  
+
+// ### Quiz Question 6:
+
+// // What will be logged to the console?
+// var foo = function() {
+//   console.log(this);
+// };
+// foo();
+
+// Options:
+// A) Window object (global context)  
+// B) undefined  
+// C) foo function object  
+// D) null  
+
+// ### Quiz Question 7:
+
+// // What will be the output?
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// Options:
+// A) 0 1 2  
+// B) 3 3 3  
+// C) 1 2 3  
+// D) undefined  
+
+// ### Quiz Question 8:
+
+// // What will be logged to the console?
+// (function() {
+//   var a = b = 5;
+// })();
+// console.log(b);
+
+// Options:
+// A) 5  
+// B) undefined  
+// C) ReferenceError  
+// D) 0  
+
+// ### Quiz Question 9:
+
+// // What will be the output?
+// var arr = [1, 2, 3];
+// arr[10] = 10;
+// console.log(arr.length);
+
+// Options:
+// A) 3  
+// B) 4  
+// C) 11  
+// D) 10  
+
+// ### Quiz Question 10:
+
+// // What will be the output?
+// var obj = {
+//   a: 1,
+//   b: 2
+// };
+// delete obj.a;
+// console.log(obj.hasOwnProperty('a'));
+
+// Options:
+// A) true  
+// B) false  
+// C) ReferenceError  
+// D) TypeError  
+
