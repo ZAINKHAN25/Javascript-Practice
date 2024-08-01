6a8105260618d1232f046381a7ba8c5cba854547
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-08-01 18:01:01 +0500

    Code khud hi samajh nahi arha

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
-// What is the result of the following expression?
+const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-2 + "2"
+function CreateDate(monthsDays, perMonth, perWeek) {
+  let ans = [];
+  let currentWeek = [];
+  let weeks = [];
 
-// Options:
-// A) "22"  B) 4  C) "4"  D) 22
+  // Generate weeks
+  for (let i = 0; i < monthsDays; i++) {
+    const dayIndex = i % 7;
+    currentWeek.push({
+      day: days[dayIndex],
+      date: i + 1,
+    });
+
+    if (dayIndex === 6 || i === monthsDays - 1) {
+      weeks.push(currentWeek);
+      currentWeek = [];
+    }
+  }
+
+  // Generate unique random week indices
+  let uniqueWeekIndices = new Set();
+  while (uniqueWeekIndices.size < perWeek) {
+    const randomWeekNum = Math.floor(Math.random() * weeks.length);
+    uniqueWeekIndices.add(randomWeekNum);
+  }
+
+  let selectedDays = [];
+  
+  // For each selected week, pick random days
+  uniqueWeekIndices.forEach(weekIndex => {
+    const week = weeks[weekIndex];
+    let uniqueDayIndices = new Set();
+    
+    while (uniqueDayIndices.size < perMonth) {
+      const randomDayNum = Math.floor(Math.random() * week.length);
+      uniqueDayIndices.add(randomDayNum);
+    }
+    
+    uniqueDayIndices.forEach(dayIndex => {
+      selectedDays.push(week[dayIndex]);
+    });
+  });
+
+  // Shuffle the days to randomize their order
+  for (let i = selectedDays.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [selectedDays[i], selectedDays[j]] = [selectedDays[j], selectedDays[i]];
+  }
+
+  return selectedDays;
+}
+
+console.log(CreateDate(31, 3, 1));
